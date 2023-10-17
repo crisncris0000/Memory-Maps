c9092f5db9d0536b85ed17af93536ed10b4da8c9
crisncris0000 <christopherrivera384@gmail.com>
2023-10-16 20:01:27 -0400

    user can now place multiple markers

--- fe-app/src/components/Maps/Maps.jsx
+++ fe-app/src/components/Maps/Maps.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map, { Marker } from 'react-map-gl';
 import '../../css/map.css';
 
 export default function Maps() {
+  const [markers, setMarkers] = useState([]);
+
+  const addMarker = (event) => {
+    const longitude = event.lngLat.lng;
+    const latitude = event.lngLat.lat;
+
+    setMarkers([...markers, { longitude, latitude }]);
+  };
+
   return (
-      <Map
-          mapboxAccessToken={process.env.REACT_APP_MAPS_API_KEY}
-          initialViewState={{
-            longitude: -100,
-            latitude: 40,
-            zoom: 3.5
-          }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          style={{width: "100vw", height: "100vh"}}
-      >
-          <Marker longitude={-100} latitude={40} offsetTop={-20} offsetLeft={-10}> 
-          </Marker>
-      </Map>
-  )
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPS_API_KEY}
+      initialViewState={{
+        longitude: -100,
+        latitude: 40,
+        zoom: 3.5
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      style={{ width: "100vw", height: "100vh" }}
+      onClick={addMarker}
+    >
+      {markers.map((marker, index) => (
+        <Marker key={index} longitude={marker.longitude} latitude={marker.latitude} offsetTop={-20} offsetLeft={-10} />
+      ))}
+    </Map>
+  );
 }
