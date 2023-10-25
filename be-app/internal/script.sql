7d0204951eb84b95695ef94c894c07100e31e231
crisncris0000 <christopherrivera384@gmail.com>
2023-10-25 13:42:33 -0400

    Worked on sql script

--- be-app/internal/script.sql
+++ be-app/internal/script.sql
@@ -1,33 +1,49 @@
-CREATE TABLE 'Users' (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100),
-    password TEXT NOT NULL,
-    role_id INT NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-);
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS Comments;
+DROP TABLE IF EXISTS MarkerPost;
+DROP TABLE IF EXISTS Users;
 
-CREATE TABLE 'Role' (
+DROP SCHEMA IF EXISTS NostalgiaMaps;
+
+CREATE SCHEMA NostalgiaMaps;
+USE NostalgiaMaps;
+
+CREATE TABLE Roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     role_name VARCHAR(50)
-)
+);
 
-CREATE TABLE 'Comments' (
+CREATE TABLE Users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    comment TEXT NOT NULL,
-    likes INT NOT NULL
-)
+    email VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES Roles(id)
+);
 
-CREATE TABLE 'MarkerPost' (
+CREATE TABLE MarkerPost (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    longtitude FLOAT NOT NULL, 
+    longitude FLOAT NOT NULL,
     latitude FLOAT NOT NULL,
-    image BYTE NOT NULL,
+    image BLOB NOT NULL,
     description TEXT NOT NULL,
     likes INT NOT NULL,
-    marker_id INT NOT NULL,
     user_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-)
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    marker_id INT NOT NULL,
+    comment TEXT NOT NULL,
+    likes INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (marker_id) REFERENCES MarkerPost(id)
+);
