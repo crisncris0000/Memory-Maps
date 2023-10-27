b4ae7872c356794fca012d4adc901e545d580991
crisncris0000 <christopherrivera384@gmail.com>
2023-10-27 10:25:51 -0400

    started user model

---
+++ be-app/internal/model/user.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID         int       `json:"id" db:"id"`
+	Email      string    `json:"email" db:"email"`
+	Password   string    `json:"password" db:"password"`
+	Role_id    int       `json:"role_id" db:"role_id"`
+	Created_at time.Time `json:"created_at" db:"created_at"`
+	Updated_at time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type UserModel interface {
+}
+
+type UserModelImpl struct {
+	*sql.DB
+}
+
+func newUserModel(db *sql.DB) *UserModelImpl {
+	return &UserModelImpl{DB: db}
+}
