e11136ab72690ba41ae675806690b5d271e0033c
crisncris0000 <christopherrivera384@gmail.com>
2023-11-08 12:32:48 -0500

    successfully added coordinates and changed map loading

--- fe-app/src/components/Maps/Maps.jsx
+++ fe-app/src/components/Maps/Maps.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Map } from 'react-map-gl';
+import React, { useEffect, useRef } from 'react';
 import '../../css/map.css'
+import mapboxgl from 'mapbox-gl';
 import { SearchBox } from '@mapbox/search-js-react'
 
 
 export default function Maps() {
+    const mapContainerRef = useRef(null);
+    const mapRef = useRef(null)
+
+    const handleOnRetrieve = (result) => {
+       const coords = result.features[0].geometry.coordinates
+
+       const longitude = coords[0]
+       const latitude = coords[1]
+
+       console.log(longitude, latitude)
+    }
+
+    useEffect(() => {
+
+        mapboxgl.accessToken= process.env.REACT_APP_MAPS_API_KEY;
+
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: "mapbox://styles/mapbox/streets-v12",
+        })
+        mapRef.current = map
+    }, [])
+
+
     return (
         <>
             <div className="search-box-container">
-                <SearchBox accessToken={process.env.REACT_APP_MAPS_API_KEY} 
-                value=''/>
+                <SearchBox
+                accessToken={process.env.REACT_APP_MAPS_API_KEY} 
+                value=''
+                onRetrieve={handleOnRetrieve}/>
             </div>
             <div className="map-container">
                 <div className="map-content">
-                    <Map mapboxAccessToken={process.env.REACT_APP_MAPS_API_KEY}
-                        mapStyle="mapbox://styles/mapbox/streets-v9"
-                        style={{width: 1000, height: 700}}>
-                    </Map>
+                    <div ref={mapContainerRef}
+                    style={{width: 1000, height: 700}}></div>
                 </div>
             </div>
         </>
