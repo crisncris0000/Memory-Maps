eae345c281bcceca520fad30b765d3e255d1cffd
crisncris0000 <christopherrivera384@gmail.com>
2023-11-30 17:53:48 -0500

    Added links to registration page

--- fe-app/src/components/Authentication/Register.jsx
+++ fe-app/src/components/Authentication/Register.jsx
@@ -1,53 +1,40 @@
 import React from 'react';
-import '../../css/register.css'
-
+import '../../css/register.css';
+import { Link } from 'react-router-dom';
+ 
 export default function Register() {
     return (
         <>
-<div className="register-container">
-      <h2>Register</h2>
-      <form>
-        <div className="form-group">
-          <label className="label">First Name</label>
-          <input
-            type="text"
-            className="input"
-            placeholder="Enter your first name"
-          />
-        </div>
+            <div className="register-container">
+                <h2>Register</h2>
+                <form>
+                    <div className="form-group">
+                        <label className="label">First Name</label>
+                        <input type="text" className="input" placeholder="Enter your first name"/>
+                    </div>
 
-        <div className="form-group">
-          <label className="label">Last Name</label>
-          <input
-            type="text"
-            className="input"
-            placeholder="Enter your last name"
-          />
-        </div>
+                    <div className="form-group">
+                        <label className="label">Last Name</label>
+                        <input type="text" className="input" placeholder="Enter your last name"/>
+                    </div>
 
-        <div className="form-group">
-          <label className="label">Email</label>
-          <input
-            type="email"
-            className="input"
-            placeholder="Enter your email"
-          />
-        </div>
+                    <div className="form-group">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Enter your email"/>
+                    </div>
 
-        <div className="form-group">
-          <label className="label">Password</label>
-          <input
-            type="password"
-            className="input"
-            placeholder="Enter your password"
-          />
-        </div>
+                    <div className="form-group">
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Enter your password"/>
+                    </div>
 
-        <button type="submit" className="button">
-          Register
-        </button>
-      </form>
-    </div>
+                    <button type="submit" className="button">
+                        Register
+                    </button>
+                </form>
+                <Link to="/" style={{"margin": "20px"}}>Return to Home</Link>
+                <Link to="/login">Return to Login</Link>
+            </div>
         </>
     )
 }
