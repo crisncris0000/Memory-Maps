50c545a6bbcb015563db350d2cfd7c2e9c433868
crisncris0000 <christopherrivera384@gmail.com>
2023-12-18 12:49:20 -0500

    added start and end dates

--- fe-app/src/components/Maps/Calender.jsx
+++ fe-app/src/components/Maps/Calender.jsx
@@ -4,17 +4,27 @@ import { zhCN } from '@mui/x-date-pickers/locales';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 export default function Calender() {
-    const [selectedDate, setSelectedDate] = React.useState(new Date());
+    const [selectedStartDate, setSelectedStartDate] = React.useState(new Date());
+    const [selectedEndDate, setSelectedEndDate] = React.useState(new Date());
 
-    const handleChange = (date) => {
-      setSelectedDate(date);
+    const handleStartDate = (date) => {
+      setSelectedStartDate(date);
+    };
+
+    const handleEndDate = (date) => {
+      setSelectedEndDate(date);
     };
   
     return (
       <LocalizationProvider dateAdapter={AdapterDateFns} locale={zhCN}>
         <DatePicker
-          value={selectedDate}
-          onChange={handleChange}
+          value={selectedStartDate}
+          onChange={handleStartDate}
+        />
+
+        <DatePicker
+          value={selectedEndDate}
+          onChange={handleEndDate}
         />
       </LocalizationProvider>
     );
