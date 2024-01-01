8d7c353a8c6d4c76a0c5c15280f6f7e0dde68765
crisncris0000 <christopherrivera384@gmail.com>
2024-01-01 15:28:46 -0500

    Working on comments function

--- fe-app/src/components/Maps/MarkerInfo.jsx
+++ fe-app/src/components/Maps/MarkerInfo.jsx
@@ -3,9 +3,21 @@ import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 
-// ... (other imports and code)
 
 export default function MarkerInfo({ show, setShow, markerPost }) {
+  const [showComments, setShowComments] = useState(false);
+
+  return (
+    <>
+      {showComments === false ? 
+      <MarkerPost show={show} setShow={setShow} markerPost={markerPost} showComments={showComments} setShowComments={setShowComments}/> 
+      : 
+      <MarkerComments show={show} setShow={setShow} showComments={showComments} setShowComments={setShowComments}/>}
+    </>
+  );
+}
+
+function MarkerPost({ show, setShow, markerPost, setShowComments }) {
 
   const handleClose = () => setShow(false);
 
@@ -52,12 +64,12 @@ export default function MarkerInfo({ show, setShow, markerPost }) {
 
           <Modal.Footer>
             <div className="likes-comments">
-              <div className="likes">
-                <span role="img" aria-label="likes">👍</span> {markerPost.likes}
-              </div>
-              <div className="comments">
-                <span role="img" aria-label="comments">💬</span> {markerPost.comments}
-              </div>
+              <button className="likes" style={{backgroundColor: "transparent"}}>
+                <span role="img" aria-label="likes">👍</span> {markerPost.likes} Likes
+              </button>
+              <button className="comments" style={{backgroundColor: "transparent"}} onClick={() => setShowComments(true)}>
+                <span role="img" aria-label="comments">💬</span> {markerPost.comments} Comments
+              </button>
             </div>
           </Modal.Footer>
         </Modal>
@@ -66,3 +78,24 @@ export default function MarkerInfo({ show, setShow, markerPost }) {
   );
 }
 
+function MarkerComments ({ show, setShow, showComments, setShowComments }) {
+
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      {show &&
+        <Modal show={show} onHide={handleClose} animation={false} className="location-modal mx-auto" centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Marker Comments</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          </Modal.Body>
+
+          <Modal.Footer>
+          </Modal.Footer>
+        </Modal>
+      }
+    </>
+  );
+}
