2c7f26ba1c53f00b4f87c154578fa876199500eb
crisncris0000 <christopherrivera384@gmail.com>
2024-01-07 19:50:28 -0500

    added router to reset token

---
+++ be-app/internal/routes/resettoken.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/crisncris0000/Memory-Maps/be-app/internal/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+type ResetTokenRouter struct {
+	ResetTokenHandler *handlers.ResetTokenHandler
+}
+
+func NewResetTokenRouter(resetTokenHandler *handlers.ResetTokenHandler) *ResetTokenRouter {
+	return &ResetTokenRouter{ResetTokenHandler: resetTokenHandler}
+}
+
+func (rtRouter *ResetTokenRouter) InitializeRouter(router *gin.Engine) {
+	router.GET("/reset-token", rtRouter.ResetTokenHandler.GetResetToken)
+	router.POST("reset-token/new", rtRouter.ResetTokenHandler.CreateResetToken)
+	router.DELETE("/reset-token/delete/:id", rtRouter.ResetTokenHandler.DeleteResetToken)
+}
